9cc42e218276a0c99a5e25d8e10b67c2d7a71217
Zoltan <zoltan@users.noreply.github.com>
2026-05-23 18:13:23 +0200

    Change server host to 0.0.0.0 for local network access

--- server.py
+++ server.py
@@ -76,4 +76,4 @@ async def add_to_catalog(building: CatalogBuilding):
     return {"status": "ok"}
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
